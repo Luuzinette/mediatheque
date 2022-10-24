43de6b35b85b9a1e06679b7de1bea19e110437df
Morgane RAVALLEC <ravallec.morgane@gmail.com>
2022-10-24 16:31:44 +0200

    Ajout BorrowController avec MakeABorrow

--- src/main/java/com/plb/mediatheque/controller/BorrowController.java
+++ src/main/java/com/plb/mediatheque/controller/BorrowController.java
@@ -1,16 +1,44 @@
 package com.plb.mediatheque.controller;
 
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.plb.mediatheque.entity.Borrow;
+import com.plb.mediatheque.entity.Item;
+import com.plb.mediatheque.entity.Users;
 import com.plb.mediatheque.repository.UsersRepository;
+import com.plb.mediatheque.service.BorrowLimitException;
 import com.plb.mediatheque.service.BorrowService;
+import com.plb.mediatheque.service.DocNotAvailableException;
 
+@RestController
+@RequestMapping("/api/borrows")
 public class BorrowController {
+	
+	@Autowired
+	BorrowService borrowService;
 	
 	@Autowired
 	UsersRepository userRepository;
 	
-	@Autowired
-	BorrowService borrowService;
+	@PostMapping("/users/{idUser}/emprunter")
+	public ResponseEntity<Borrow> makeBorrow(@PathVariable Long idUser, @RequestBody Set<Item> items) throws BorrowLimitException, DocNotAvailableException {
+		Users borrower = new Users();
+		borrower = userRepository.findById(idUser).orElseThrow(() -> new EntityNotFoundException("Cet utilisateur n'existe pas."));
+		borrower.setId(idUser);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(borrowService.makeABorrow(borrower, items));
+	}
 
 }
