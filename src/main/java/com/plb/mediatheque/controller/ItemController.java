8719c81ab6e1f84853df30ff7ce875d1b430561f
Morgane RAVALLEC <ravallec.morgane@gmail.com>
2022-10-24 15:30:47 +0200

    Ajout ItemController

--- src/main/java/com/plb/mediatheque/controller/ItemController.java
+++ src/main/java/com/plb/mediatheque/controller/ItemController.java
@@ -1,12 +1,42 @@
 package com.plb.mediatheque.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.plb.mediatheque.entity.Item;
 import com.plb.mediatheque.repository.ItemRepository;
 
+@RestController
+@RequestMapping("/api/items")
 public class ItemController {
 	
 	@Autowired
 	ItemRepository itemRepository;
-
+	
+	@GetMapping()
+	public ResponseEntity<List<Item>> getAllItems(){
+		return new ResponseEntity<>(itemRepository.findAll(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/dvd")
+	public ResponseEntity<List<Item>> getAllDvd(){
+		return new ResponseEntity<>(itemRepository.findAllDvd(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/cd")
+	public ResponseEntity<List<Item>> getAllCd(){
+		return new ResponseEntity<>(itemRepository.findAllCd(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/book")
+	public ResponseEntity<List<Item>> getAllBook(){
+		return new ResponseEntity<>(itemRepository.findAllBook(), HttpStatus.OK);
+	}
+	
 }
