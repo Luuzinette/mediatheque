b3bcc324f5889e4c0c6e108bc0c8591a38d527c3
Morgane RAVALLEC <ravallec.morgane@gmail.com>
2022-10-25 11:48:28 +0200

    Complète Swagger avec tous Emprunts par utilisateur

--- src/main/java/com/plb/mediatheque/controller/UsersController.java
+++ src/main/java/com/plb/mediatheque/controller/UsersController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.plb.mediatheque.entity.Borrow;
 import com.plb.mediatheque.entity.Users;
 import com.plb.mediatheque.repository.BorrowRepository;
 import com.plb.mediatheque.repository.UsersRepository;
@@ -24,6 +26,19 @@ public class UsersController {
 	@GetMapping()
 	public List<Users> findAllUser(){
 		return userRepository.findAll();
+	}
+	
+	@GetMapping("/{idUser}")
+	public List<Users> findOne(@PathVariable Long idUser) {
+		return userRepository.findOneUser(idUser); 
+	}
+	
+	@GetMapping("/{idUser}/emprunts")
+	public List<Borrow> findBorrowsById(@PathVariable Long idUser) {
+		userRepository.findOneUser(idUser); 
+		Users user = new Users();
+		user.setId(idUser);
+		return borrowRepository.findBorrowsById(user); 		
 	}
 
 }
--- src/main/java/com/plb/mediatheque/repository/UsersRepository.java
+++ src/main/java/com/plb/mediatheque/repository/UsersRepository.java
@@ -1,9 +1,15 @@
 package com.plb.mediatheque.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.plb.mediatheque.entity.Users;
 
 public interface UsersRepository extends JpaRepository<Users, Long> {
+	
+	@Query("from Users u where u.id = :id")
+	public List<Users> findOneUser(Long id);
 
 }
